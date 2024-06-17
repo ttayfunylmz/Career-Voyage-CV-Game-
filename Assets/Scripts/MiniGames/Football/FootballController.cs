faf20802a139d68f511de5e0250778900cc2c342
ttayfunylmz <tyfnylmz201974@gmail.com>
2024-06-17 04:48:28 +0300

    v0.9 Particles Added to Mini Games

--- Assets/Scripts/MiniGames/Football/FootballController.cs
+++ Assets/Scripts/MiniGames/Football/FootballController.cs
@@ -10,12 +10,15 @@ public class FootballController : MonoBehaviour
     [SerializeField] private Rigidbody footballRigidbody;
     [SerializeField] private TMP_Text leftScoreText;
     [SerializeField] private TMP_Text rightScoreText;
+    [SerializeField] private ParticleSystem leftConfettiParticles;
+    [SerializeField] private ParticleSystem rightConfettiParticles;
 
     [Header("Settings")]
-    [SerializeField] private int leftScore = 0;
-    [SerializeField] private int rightScore = 0;
-    [SerializeField] private Vector3 startPosition;
     [SerializeField] private float waitingSeconds = 2f;
+
+    private int leftScore = 0;
+    private int rightScore = 0;
+    private Vector3 startPosition;
 
     private void Awake() 
     {
@@ -37,28 +40,35 @@ public class FootballController : MonoBehaviour
     private void OnRightGoalEntered()
     {
         IncreaseScore(ref rightScore, leftScoreText);
+        PlayConfetti(rightConfettiParticles);
         StartCoroutine(ResetFootball());
     }
 
     private void OnLeftGoalEntered()
     {
         IncreaseScore(ref leftScore, rightScoreText);
+        PlayConfetti(leftConfettiParticles);
         StartCoroutine(ResetFootball());
     }
 
     private void IncreaseScore(ref int score, TMP_Text text)
     {
-        AudioManager.Instance.Play(Consts.Sounds.CONFETTI_SOUND);
         score++;
         text.text = score.ToString();
     }
 
+    private void PlayConfetti(ParticleSystem confettiParticles)
+    {
+        confettiParticles.Play();
+        AudioManager.Instance.Play(Consts.Sounds.CONFETTI_SOUND);
+    }
+
     private IEnumerator ResetFootball()
     {
+        footballRigidbody.isKinematic = true;
         yield return new WaitForSeconds(waitingSeconds);
 
         AudioManager.Instance.Play(Consts.Sounds.WHISTLE_SOUND);
-        footballRigidbody.isKinematic = true;
         footballTransform.position = startPosition;
         yield return null;
 
